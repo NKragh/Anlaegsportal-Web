0df2b8e3d0b665f8dcb0c615144e766e9d4ff111
NKragh <39466838+NKragh@users.noreply.github.com>
2020-09-17 13:31:40 +0200

    Created new js class and other stuff

--- mockup.js
+++ mockup.js
@@ -1,3 +1,11 @@
+
+function classtest() {
+test = new Installation("Test")
+
+console.log(test.Name)
+}
+document.onload = setlinks()
+
 function navigate(input) {
   switch (input) {
     case "address":
@@ -19,7 +27,7 @@ function search() {
 
   console.log(value)
 
-  fetch(`https://dawa.aws.dk/adresser/autocomplete?q=${value}&fuzzy=`)
+  fetch(`https://dawa.aws.dk/adresser/autocomplete?q=${value} &fuzzy=`)
     .then(response => response.json())
     .then(data => {
       // console.log(data)
@@ -50,5 +58,11 @@ function changemap(data) {
   kort.src = str
 }
 
-//https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2252.522478317918!2d12.080531515925676!3d55.627724280520056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46525fe9a8b9ee3f%3A0xc1b98f0c07aa237b!2sPenselstr%C3%B8get%2030%2C%204000%20Roskilde!5e0!3m2!1sda!2sdk!4v1600252510578!5m2!1sda!2sdk
-//https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2252.5101266681727!2d12.079593316561274!3d55.62793918052008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46525fe9a9327fa7%3A0x47b841097d1bbc90!2sPenselstr%C3%B8get%2066%2C%204000%20Roskilde!5e0!3m2!1sda!2sdk!4v1600252896720!5m2!1sda!2sdk
+var liste = []
+
+function setlinks() {
+  liste = document.getElementsByName('installationLink')
+  liste.forEach(l => {
+    l.href = "mockup-installation.html"
+  });
+}
