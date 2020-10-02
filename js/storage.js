79c73bd03988403c60cfdf4a35b2b962064c9c7a
NKragh <39466838+NKragh@users.noreply.github.com>
2020-10-02 09:32:05 +0200

    sastoken updated

--- js/storage.js
+++ js/storage.js
@@ -1,4 +1,4 @@
-const sastoken = "sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-10-01T21:22:58Z&st=2020-10-01T13:22:58Z&spr=https&sig=eDiAdhrKLRHseWahdEvCFLxV5iFNuOy81D6bltIsaSw%3D"
+const sastoken = "st=2020-10-02T07%3A15%3A36Z&se=2021-01-30T23%3A00%3A00Z&sp=rl&sv=2018-03-28&sr=s&sig=nxy8kWzTC5%2B93yz0OAgMkxh897TTPs81i1%2FFKANkwKI%3D"
 const getdocuments_url = "https://anlaegsportalstorage.file.core.windows.net/documents";
 const getimage_url ="https://anlaegsportalstorage.file.core.windows.net/images";
 
