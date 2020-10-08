8207a05704030c574ec6d34ccd4f70510e267b36
NKragh <39466838+NKragh@users.noreply.github.com>
2020-10-08 09:56:37 +0200

    Restructuring and address comes from REST now

--- js/Installation.js
+++ js/Installation.js
@@ -6,99 +6,58 @@ import {
 document.onload = GetInformation()
 
 async function GetInformation() {
-  var data = [];
-
   var ref = document.referrer
-  console.log(ref.split('/'))
 
   if (ref.split('/')[3] == "address.html") {
-    myfetch('address')
-      .then(result => {
-        const inst = result
-        localStorage.setItem('qrid', inst['qrId'])
-        console.log(inst)
-        for (const key in inst) {
-          if (inst.hasOwnProperty(key)) {
-            const val = inst[key];
-            // console.log(key, val)
-            var temp = document.getElementById(key)
-            if (key == 'bbrId') {
-              fetch(`https://dawa.aws.dk/adresser/${val}`)
-                .then(response => response.json())
-                .then(data => {
-                  temp = document.getElementById(key)
-
-                  var address = data['adressebetegnelse']
-
-                  temp.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${address}`
-                  temp.href = `https://www.google.com/maps/place/${data['adressebetegnelse']}/`
-                })
-            } else if (key == 'qrId') {
-              continue
-            } else {
-              temp.innerText = val
-            }
-          }
-        }
-        var picele = document.getElementById('infopic')
-        picele.src = `${getimage_url}/${inst.installationId}.jpg`
-      })
-  } else {
-    myfetch('qrid') //
-      .then(data => {
-        const inst = data
-        localStorage.setItem('installationId', inst.installationId)
-        localStorage.setItem('bbrid', inst.bbrId)
-        console.log(inst)
-        for (const key in inst) {
-          if (inst.hasOwnProperty(key)) {
-            var temp;
-            const val = inst[key];
-            // console.log(key, val)
-            if (key == 'bbrId') {
-              fetch(`https://dawa.aws.dk/adresser/${val}`)
-                .then(response => response.json())
-                .then(data => {
-                  temp = document.getElementById(key)
-
-                  var address = data['adressebetegnelse']
-
-                  temp.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${address}`
-                  temp.href = `https://www.google.com/maps/place/${data['adressebetegnelse']}/`
-                })
-            } else if (key == 'qrId') {
-              continue
-            } else {
-              temp = document.getElementById(key)
-              temp.innerText = val
-            }
-          }
-        }
-        var picele = document.getElementById('infopic')
-        picele.src = `${getimage_url}/${inst.installationId}.jpg`
-
-      })
-  }
-}
-
-async function myfetch(q) {
-  if (q == 'address') {
     const installationId = localStorage.getItem('installationId');
 
-    var response = await fetch(`${rest_url}${installationId}`)
-    var data = await response.json()
-    return data
+    fetch(`${rest_url}/${installationId}`)
+      .then(response => response.json())
+      .then(data => {
+        const inst = data
+        localStorage.setItem('qrid', inst['qrId'])
+        for (const key in inst) {
+          if (inst.hasOwnProperty(key)) {
+            const val = inst[key];
+            var temp = document.getElementById(key)
+            if (key == 'address') {
+              temp.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${val}`
+              temp.href = `https://www.google.com/maps/place/${val}/`
+            } else if (key == 'qrId' || key == 'bbrId') {
+              continue
+            } else {
+              temp.innerText = val
+            }
+          }
+        }
+        var picele = document.getElementById('infopic')
+        picele.src = `${getimage_url}/${inst.installationId}.jpg`
+      })
   } else {
-    try {
-      var qrid = localStorage.getItem('qrid')
-    } catch (error) {
-      var qrid = ""
-      console.log(error)
-    }
+    var qrid = localStorage.getItem('qrid')
+    fetch(`${rest_url}/search?qrid=${qrid}`)
+      .then(response => response.json())
+      .then(data => {
+        const inst = data[0]
+        localStorage.setItem('installationId', inst.installationId)
+        localStorage.setItem('bbrid', inst.bbrId)
+        for (const key in inst) {
+          if (inst.hasOwnProperty(key)) {
+            var temp = document.getElementById(key)
+            const val = inst[key];
+            if (key == 'address') {
+              temp.innerHTML = `<i class="fas fa-map-marker-alt"></i> ${val}`
+              temp.href = `https://www.google.com/maps/place/${val}/`
+            } else if (key == 'qrId' || key == 'bbrId') {
+              continue
+            } else {
+              temp.innerText = val
+            }
+          }
+        }
+        var picele = document.getElementById('infopic')
+        picele.src = `${getimage_url}/${inst.installationId}.jpg`
 
-    var response = await fetch(`${rest_url}search?qrid=${qrid}`)
-    var data = await response.json()
-    return data[0]
-
+      })
   }
 }
--- js/storage.js
+++ js/storage.js
@@ -1,10 +1,15 @@
 const sastoken = "st=2020-10-02T07%3A15%3A36Z&se=2021-01-30T23%3A00%3A00Z&sp=rl&sv=2018-03-28&sr=s&sig=nxy8kWzTC5%2B93yz0OAgMkxh897TTPs81i1%2FFKANkwKI%3D"
 const getdocuments_url = "https://anlaegsportalstorage.blob.core.windows.net/documents";
-const getimage_url ="https://anlaegsportalstorage.blob.core.windows.net/images";
+const getimage_url = "https://anlaegsportalstorage.blob.core.windows.net/images";
 
-const rest_url = "https://lekondbrest.azurewebsites.net/api/installations/"
+const rest_url = "https://lekondbrest.azurewebsites.net/api/installations"
 
-export {sastoken, getdocuments_url, getimage_url, rest_url};
+export {
+  sastoken,
+  getdocuments_url,
+  getimage_url,
+  rest_url
+};
 
 //sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-10-01T21:22:58Z&st=2020-10-01T13:22:58Z&spr=https&sig=eDiAdhrKLRHseWahdEvCFLxV5iFNuOy81D6bltIsaSw%3D
 //sv=2019-12-12&ss=bfqt&srt=sco&sp=rwdlacupx&se=2020-10-01T21:22:58Z&st=2020-10-01T13:22:58Z&spr=https&sig=eDiAdhrKLRHseWahdEvCFLxV5iFNuOy81D6bltIsaSw%3D
