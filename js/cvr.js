9bf0e28d9c1b93b8e53e325a4b90d0e3f0dce97c
NKragh <39466838+NKragh@users.noreply.github.com>
2020-10-08 17:09:10 +0200

    Very little continuation of cvr

--- js/cvr.js
+++ js/cvr.js
@@ -1,21 +1,33 @@
+document.getElementById('inputcvr').addEventListener('keydown', hentcvr)
+
 /**
  * Gets information from the CVR API based on input text
  */
 function hentcvr() {
-  result = {}
 
-  input = document.getElementById('inputcvr').value
+  if (event.key == "Enter") {
+    result = {}
 
-  fetch(`https://cvrapi.dk/api?search=${input}&country=dk`, {
-      headers: {
-        "User-agent": "Test af CVR integration til mit projekt"
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      fyldresult(data)
-    })
+    input = document.getElementById('inputcvr').value
+
+    fetch(`https://cvrapi.dk/api?search=${input}&country=dk`, {
+        headers: {
+          "User-agent": "Test af CVR integration til mit projekt"
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
+        fyldresult(data)
+        let list = document.getElementById('resultlist')
+        for (const key in data) {
+          const value = data[key];
+          list.innerHTML += `<li>${key} : ${value}</li>`
+        }
+      })
+  } else {
+    return
+  }
 }
 
 /**
@@ -23,9 +35,9 @@ function hentcvr() {
  * @param {Object} data - JSON data from CVR API
  */
 function fyldresult(data) {
-  let div = document.getElementById('resultlist')
+  let list = document.getElementById('resultlist')
   for (const key in data) {
     const value = data[key];
-    div.innerHTML += `<li>${key} : ${value}</li>`
+    list.innerHTML += `<li>${key} : ${value}</li>`
   }
 }
