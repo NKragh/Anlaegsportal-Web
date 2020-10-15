53a6e6b008f8a6f538fdd1afea3808490310b19d
NKragh <39466838+NKragh@users.noreply.github.com>
2020-10-15 16:20:06 +0200

    Upload so close to working...

--- admin/js/upload.js
+++ admin/js/upload.js
@@ -1,16 +1,29 @@
 import {
-  getdocuments_url
+  getdocuments_url,
+  local_url
 } from '../../js/storage.js'
 
 document.getElementById('uploadbtn').addEventListener('click', upload)
 
-function SendDocument() {
-  //XMLHttp POST metode, med dokumentet.
+var file;
 
+function SendDocument() {
+  var installationId = localStorage.getItem('installationId')
+  //XMLHttp POST metode, med dokumentet.
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      document.getElementById('status').innerText = "Uploaded. Redirecting in 5 sec."
+      this.timeout(5000)
+      history.back()
+    }
+  }
+  xmlhttp.open("PUT", `${getdocuments_url}/${installationId}/${file.name}`, true)
+  xmlhttp.send(file)
 }
 
 function upload() {
-  const file = document.getElementById('myFile').files[0]
-
+  file = document.getElementById('myFile').files[0]
+  SendDocument()
   console.log(file.name)
 }
