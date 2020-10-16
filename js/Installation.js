d54809b7c8a388f37e26407df9db058bdfa6048a
NKragh <39466838+NKragh@users.noreply.github.com>
2020-10-16 14:24:04 +0200

    Review-button and local REST while in development

--- js/Installation.js
+++ js/Installation.js
@@ -12,7 +12,7 @@ async function GetInformation() {
   if (ref.split('/')[3] == "address.html") {
     const installationId = localStorage.getItem('installationId');
 
-    fetch(`${rest_url}/${installationId}`)
+    fetch(`${local_url}/${installationId}`)
       .then(response => response.json())
       .then(data => {
         const inst = data
@@ -40,7 +40,7 @@ async function GetInformation() {
       })
   } else {
     var qrid = localStorage.getItem('qrid')
-    fetch(`${rest_url}/search?qrid=${qrid}`)
+    fetch(`${local_url}/search?qrid=${qrid}`)
       .then(response => response.json())
       .then(data => {
         const inst = data[0]
